d0067003e5f9c953f8cd342e72cb7066028e40d4
pedro0527 <sooyong0527@naver.com>
2026-02-03 22:16:59 +0900

    [feat/#5] router 설정

--- src/pages/town/Town.jsx
+++ src/pages/town/Town.jsx
@@ -1,9 +1,9 @@
-import * as S from "./Town.styled";
+// import * as S from "./Town.styled";
 
 export const Town = () => {
   return (
     <>
-      <S.Wrapper></S.Wrapper>
+      <h1>Town</h1>
     </>
   );
 };
