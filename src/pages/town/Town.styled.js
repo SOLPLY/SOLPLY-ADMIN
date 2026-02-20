ce8a568d556a8a0be92e92a8d5703a60784e3af7
pedro0527 <sooyong0527@naver.com>
2026-02-20 23:50:51 +0900

    [feat/#13] 동네 관리 UI 구현

--- src/pages/town/Town.styled.js
+++ src/pages/town/Town.styled.js
@@ -3,7 +3,108 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: flex-start;
   width: 100vw;
+  height: 95vh;
+  margin-top: 5vh;
   background-color: ${({ theme}) => theme.colors.white};
+`;
+
+export const Title = styled.span`
+  font-size: 30px;
+  font-weight: 500;
+  margin-bottom: 3%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 80%;
+  padding: 2%;
+  border: 3px solid gray;
+  border-radius: 10px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const AddBtn = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+
+  padding: 10px 20px;
+  font-size: 20px;
+  font-weight: 500;
+  color: #fff;
+
+  background-color: ${({ theme }) => theme.colors.black};
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
+`;
+
+export const Table = styled.div`
+  width: 100%;
+  border: 1px solid #e5e7eb;
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
+export const TableHeader = styled.div`
+  display: grid;
+  align-items: center;     
+  justify-items: center;  
+  grid-template-columns: 10% 30% 30% 15% 15%;
+  padding: 12px;
+  font-weight: 600;
+  background-color: #f9fafb;
+  border-bottom: 1px solid #e5e7eb;
+`;
+
+export const TableRow = styled.div`
+  display: grid;
+  align-items: center;      
+  justify-items: center; 
+  grid-template-columns: 10% 30% 30% 15% 15%;
+
+  padding: 12px;
+  align-items: center;
+  border-bottom: 1px solid #e5e7eb;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const Toggle = styled.div`
+  width: 36px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: #111827;
+  position: relative;
+  cursor: pointer;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 2px;
+    left: 18px;
+    width: 16px;
+    height: 16px;
+    background-color: white;
+    border-radius: 50%;
+  }
+`;
+
+export const EditBtn = styled.button`
+  display: flex;
+  justify-content: left;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 20px;
 `;
