d7ce3fa0509aeef8d5c94e1a2305a668233c15af
pedro0527 <sooyong0527@naver.com>
2026-03-03 23:40:57 +0900

    장소 관리 UI 구현

--- src/pages/BasicStructure/Basic.jsx
+++ src/pages/BasicStructure/Basic.jsx
@@ -3,6 +3,7 @@ import * as S from "./Basic.styled";
 import TabBar from "../../components/TabBar";
 
 import Town from "../town/Town";
+import Place from "../place/Place";
 
 const Basic = () => {
   const [activeTab, setActiveTab] = useState("TOWN");
@@ -12,7 +13,7 @@ const Basic = () => {
       <TabBar activeTab={activeTab} onChangeTab={setActiveTab} />
       <S.Content>
         {activeTab === "TOWN" && <Town />}
-        {activeTab === "PLACE" && <div>장소 관리</div>}
+        {activeTab === "PLACE" && <Place />}
         {activeTab === "COURSE" && <div>코스 관리</div>}
         {activeTab === "TAG" && <div>태그 관리</div>}
         {activeTab === "PLACE_APPROVAL" && <div>등록 장소 승인</div>}
